f00dccb14f5e2567eca1596a630a8ba0dda391a8
praveen-me <praveenkrsaini0@gmail.com>
2019-05-05 18:31:35 +0530

    Add simple desktop navbar

--- src/components/DesktopMenu.js
+++ src/components/DesktopMenu.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
+import './../desktop.css';
 
+// TODO:
 // Line 18 - User Defined attributes for conditional rendering
+// 1 - Use index instead of i
+// 2 - Proper Spacing
 
 class DesktopMenu extends Component {
   render() {
-    const { children } = this.props;
+    const { children, direction } = this.props;
+  
+    let navClass = '';
+    switch ( direction ) {
+      case "right": navClass = 'rmm-desktop--right'
+      break;
+
+      case "left": navClass = 'rmm-desktop--left'
+      break;
+
+      default: navClass = '';
+    }
+
     return (
       <div>
-        <nav>
-          <ul>
+        <nav className="rmm-desktop__nav">
+          <ul className={`rmm-desktop__list ${ navClass }`}>
             {
               children.map( (child, i) => (
-                <li key={ child.props.href + i }>
-                  <a href={child.props.href}
-                  style = {
-                    {
-                      backgroundColor: child.props['data-success'] ? 'red' : 'none'
-                    }
-                  }
-                  >{child.props.children} </a>
+                <li 
+                key={ child.props.href + i } className="rmm-desktop__item">
+                  <a 
+                  className="rmm-desktop-link" 
+                  href={ child.props.href } >{ child.props.children } </a>
                 </li>
               ))
             }
--- src/components/DesktopMenuSection.js
+++ src/components/DesktopMenuSection.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DesktopMenu from "./DesktopMenu";
 import { menuData } from '../menuData';
 
 
-class DesktopMenuSection extends React.Component {
-
-	state = {
-		name : 'Praveen',
-	};
-
+class DesktopMenuSection extends Component {
 	render() {
 		return(
 			<DesktopMenu>
 				{
 					menuData.map( ( data, i ) => (
-						<a href={ data.url + i } className={ data.className } key={ data.url }>{ data.label }</a>
+						<a href={ data.url + i } 
+						className={ data.className } 
+						key={ data.url }>{ data.label }</a>
 					))
 				}
 			</DesktopMenu>
