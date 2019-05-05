b70c4a0e07157d5414835aa7cc586cf2c0073031
Imran Sayed <imranhsayed@gmail.com>
2019-05-05 12:01:38 +0530

    Add menu data

---
+++ src/menuData.js
@@ -0,0 +1,44 @@
+const menuData = [
+	{
+		label: 'Home',
+		url: 'https://imransayed.com',
+		className: 'rmm-list',
+	},
+
+	{
+		label: 'About',
+		url: 'https://imransayed.com',
+		className: 'rmm-list',
+	},
+
+	{
+		label: 'Blogs',
+		url: 'https://imransayed.com',
+		className: 'rmm-list',
+	},
+
+	{
+		label: 'Contact Us',
+		url: 'https://imransayed.com',
+		className: 'rmm-list',
+	},
+
+	{
+		label: 'Reviews',
+		url: 'https://imransayed.com',
+		className: 'rmm-list',
+	},
+
+	{
+		label: 'Sign Up',
+		url: 'https://imransayed.com',
+		className: 'rmm-list',
+	},
+
+	{
+		label: 'Sign In',
+		url: 'https://imransayed.com',
+		className: 'rmm-list',
+	},
+
+];
