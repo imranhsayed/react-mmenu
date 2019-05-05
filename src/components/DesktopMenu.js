6d656369511eb09599e36fb7c5e39ddc90b05362
praveen-me <praveenkrsaini0@gmail.com>
2019-05-05 12:57:11 +0530

    Initialized files

---
+++ src/components/DesktopMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DesktopMenu extends Component {
+  render() {
+    return (
+      <div>
+        Hello from Desktop Menu
+      </div>
+    )
+  }
+};
+
+export default DesktopMenu;
